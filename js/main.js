29c1b7497482da722549b407a2b540c2c6717a47
MuhammadNouman96 <muhammadnouman273@gmail.com>
2021-01-04 16:03:12 +0500

    seconf changes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
 
 $(document).ready(function(){
-  
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true,
-        },
-        600:{
-            items:2,
-            nav:true,
-        },
-        1000:{
-            items:3,
-            nav:true,
-            loop:false
-        }
-    }
-})
-})
-$(document).ready(function(){
     $("#demi").click(function(){
         $("#div-1").toggle();
     });
